dda91d754810562cc84382f113707f14c6ac4e59
Fahira <fahira1678@gmail.com>
2021-11-25 15:58:32 +0700

    Initial commit

---
+++ P3_1_1204044/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace P3_1_1204044
+{
+    partial class OutputLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(14, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(Hapus text yang ada dan biarkan kosong)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // HelloButton
+            // 
+            this.HelloButton.Location = new System.Drawing.Point(57, 87);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(75, 25);
+            this.HelloButton.TabIndex = 1;
+            this.HelloButton.Text = "&Say Hello";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.HelloButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(161, 87);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 25);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // OutputLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(284, 136);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.HelloButton);
+            this.Controls.Add(this.label1);
+            this.Name = "OutputLabel";
+            this.Text = "Form Hello";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button HelloButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
